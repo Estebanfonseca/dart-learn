1c9ee554c8ec54d7a9b178e3d37196e88c477d54
Estebanfonseca <estebanp57@hotmail.com>
2023-01-10 14:39:38 -0500

    condicional switch

--- holamundo/bin/game_adventure.dart
+++ holamundo/bin/game_adventure.dart
@@ -7,4 +7,23 @@ void main(List<String> args) {
   } else {
     print('no existe ');
   }
+
+  var calificacion = 'D';
+  switch (calificacion) {
+    case 'A':
+      print('Es una A');
+      break;
+    case 'B':
+      print('Es una B');
+      break;
+    case 'C':
+      print('Es una C');
+      break;
+    case 'D':
+      print('Es una D');
+      break;
+    default:
+      print('No tienes nota');
+      break;
+  }
 }
