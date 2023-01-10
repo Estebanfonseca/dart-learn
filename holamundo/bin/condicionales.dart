8cf646e18edbdd300ee1a88a9fa29f1c2fa87cca
Estebanfonseca <estebanp57@hotmail.com>
2023-01-10 14:28:44 -0500

    condicionales

---
+++ holamundo/bin/condicionales.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var a = 20;
+  var b = 15;
+
+  if (a < b) {
+    print('$b es mayor');
+  } else {
+    print('$a es mayor');
+  }
+}
---
+++ holamundo/bin/game_adventure.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  String nombre = 'esteban';
+  int edad = 24;
+
+  if (edad == 24 && nombre == 'esteban') {
+    print('hola $nombre');
+  } else {
+    print('no existe ');
+  }
+}
